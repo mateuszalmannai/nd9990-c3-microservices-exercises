b26d461c0bec66ddfac085f8c881cf127278be1e
mateusz <mateuszalmannai@googlemail.com>
2020-06-20 20:18:30 +0200

    refactor

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,13 +1,13 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while (true) {
-    const favoriteFood = process.env.FAVORITE_FOOD;
     console.log(`My favorite food is ${favoriteFood}`);
 
-    console.log('Microservices rock!');
     await sleep(5000);
   }
 }
