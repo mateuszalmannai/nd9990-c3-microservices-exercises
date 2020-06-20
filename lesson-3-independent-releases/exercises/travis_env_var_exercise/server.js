eb74e0b4ab21fd959bce9f7161a71be84d297d15
mateusz <mateuszalmannai@googlemail.com>
2020-06-20 20:12:15 +0200

    output environment variable

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,11 +1,13 @@
-
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
+  while (true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
+
+    console.log('Microservices rock!');
     await sleep(5000);
   }
 }
